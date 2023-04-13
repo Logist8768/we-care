e3b2b16a2be61fddc89e25c43d5be67ae121b229
HusseinReda97pro <pro.hussein.reda@gmail.com>
2023-04-13 08:05:55 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+const { PeerServer } = require('peer');
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.use('/peerjs', PeerServer)
+
+// app.get('/', (req, res) => {
+//   res.redirect(`/${uuidV4()}`)
+// })
+
+// app.get('/:room', (req, res) => {
+//   res.render('room', { roomId: req.params.room })
+// })
+
+app.get('/bus-1', (req, res) => {
+  res.render('room', { roomId: "bus-1" })
+})
+
+io.on('connection', socket => {
+  console.log('io  connection');
+  socket.on('join-room', (roomId, userId) => {
+    console.log(roomId, userId);
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(3000)
