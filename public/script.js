206c663c7a7a9883e98f898bc302f6d632e09973
HusseinReda97pro <pro.hussein.reda@gmail.com>
2023-04-14 06:25:17 +0200

    first commit

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: 'https://we-care.herokuapp.com',
+  host: 'we-care.herokuapp.com',
   secure: true,
   port: '443'
 })
